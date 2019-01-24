1538ad50df98aa0dbf3ca355df0ed73e02cf7a3f
Taras Didyk <tarasdidyk94@gmail.com>
2019-01-24 10:08:46 +0000

    test

--- src/common/main/Test.java
+++ src/common/main/Test.java
@@ -1,1 +1,27 @@
 
+#https://github.com/tarasdidyk/GeometricShapespackage common.main;
+
+
+public abstract class Shape {
+
+    private double area;
+    private String color;
+
+
+    public void shapeDraw() {
+
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void shapeAllInformation() {
+
+    }
+
+}
